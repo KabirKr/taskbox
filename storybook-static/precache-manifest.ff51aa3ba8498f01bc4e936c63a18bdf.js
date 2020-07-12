4a650b8c007215eb7395fa692a8155816e353738
KabirKr <kabirgogoi9@gmail.com>
2020-07-12 10:35:09 +0530

    taskbox display complete

---
+++ storybook-static/precache-manifest.ff51aa3ba8498f01bc4e936c63a18bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dea8d8fe9a0ac9bfeac6232afa6b35",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7680c3a4bf186ee11e4d.bundle.js"
+  },
+  {
+    "url": "runtime~main.7680c3a4bf186ee11e4d.bundle.js"
+  },
+  {
+    "revision": "12b16ba6a440cef472fd",
+    "url": "static/css/main.b15adadd.chunk.css"
+  },
+  {
+    "url": "vendors~main.7680c3a4bf186ee11e4d.bundle.js"
+  },
+  {
+    "url": "vendors~main.7680c3a4bf186ee11e4d.bundle.js.LICENSE.txt"
+  }
+]);
